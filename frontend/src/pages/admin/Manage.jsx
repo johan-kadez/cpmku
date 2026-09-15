06c96bab71fe8485b54af93c7731fc68173d8f48
johankadez <dafakurniawan1221@gmail.com>
2026-09-15 15:10:15 +0700

    Fix button rendering and add missing newline

--- frontend/src/pages/admin/Manage.jsx
+++ frontend/src/pages/admin/Manage.jsx
@@ -1018,6 +1018,7 @@ export default function Manage({
                   )
                   : null
               )
+              }
           >
             Detail
           </button>
@@ -1119,6 +1120,7 @@ export default function Manage({
                   Hapus
                 </button>
               )
+              }
           >
             Detail
           </button>
