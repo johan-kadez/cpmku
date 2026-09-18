e239d12d16fb38693d26744ffeb86267701c1e83
johankadez <dafakurniawan1221@gmail.com>
2026-09-18 20:08:59 +0700

    Refactor key-value serialization logic
    Removed unnecessary mapping of key-value pairs.

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -70,17 +70,13 @@ a.localeCompare(b)
 )
 .join('&');
 
-.map(
-([key, value]) =>
-"${key}=${value}"
-)
-
 return crypto
 .createHash('sha1')
 .update(
 "${serialized}${env.cloudinary.apiSecret}"
 )
 .digest('hex');
+}
 
 export const me =
 async (req, res) => {
